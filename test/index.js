56e6a2af4993e6e7504672d9179357593a9d382c
mestafor <mestalex@gmail.com>
2018-12-07 10:07:32 +0200

    Added: Test Runner

--- test/index.js
+++ test/index.js
@@ -22,12 +22,73 @@ _app.getTestCount = () => {
   return count;
 }
 
+// Centered text in the screen
+_app.centered = (str = '') => {
+  const width = process.stdout.columns;
+  const paddingLeft = (width - str.length) / 2;
+  let line = '';
+  for (let i = 0; i < paddingLeft; i++) {
+    line += ' ';
+  }
+  console.log(line + str);
+};
+
+_app.horizontalLine = () => {
+  const width = process.stdout.columns;
+  let line = '';
+  for (let i = 0; i < width; i++) {
+    line += '~';
+  }
+  console.log(line);
+};
+
+_app.verticalSpace = (n = 1) => {
+  for (let i = 0; i < n; i++) {
+    console.log();
+  }
+};
+
+// Produce a test outcome report
+_app.produceTestReport = (limit, successes, errors) => {
+  _app.verticalSpace();
+  _app.horizontalLine();
+  _app.centered('BEGIN THE REPORT');
+  _app.horizontalLine();
+  _app.verticalSpace();
+
+  console.log('\x1b[33m%s\x1b[0m \x1b[33m%s\x1b[0m', 'Total Test:', limit);
+  console.log('\x1b[32m%s\x1b[0m \x1b[32m%s\x1b[0m', 'Pass:      ', successes);
+  console.log('\x1b[31m%s\x1b[0m \x1b[31m%i\x1b[0m', 'Fail:      ', errors.length);
+
+  if (errors.length > 0) {
+    _app.verticalSpace();
+    console.log('\x1b[31m%s\x1b[0m', 'ERRORS DETAILS:');
+    _app.verticalSpace();
+    errors.forEach(error => {
+      console.log('\x1b[31m%s:\x1b[0m', error.name);
+      console.log(error.error);
+      _app.verticalSpace();
+    });
+  }
+
+  _app.verticalSpace();
+  _app.horizontalLine();
+  _app.centered('END THE REPORT');
+  _app.horizontalLine();
+  _app.verticalSpace();
+};
+
 // Run tests function
 _app.runTests = () => {
   const errors = [];
   const limit = _app.getTestCount();
   let count = 0;
   let successes = 0;
+
+  _app.horizontalLine();
+  _app.centered('Run test');
+  _app.horizontalLine();
+  _app.verticalSpace();
 
   Object.values(_app.tests)
     .forEach(subTest => {
@@ -36,7 +97,11 @@ _app.runTests = () => {
           testFn(() => {
             successes++;
             count++;
-            console.log('\x1b[32m%s\x1b[0m', testName);
+            console.log('\x1b[32m%s%s\x1b[0m', 'Passed: ', testName);
+
+            if (count === limit) {
+              _app.produceTestReport(limit, successes, errors);
+            }
           });
         } catch (e) {
           count++;
@@ -44,7 +109,11 @@ _app.runTests = () => {
             name: testName,
             error: e
           });
-          console.log('\x1b[31m%s\x1b[0m', testName);
+          console.log('\x1b[31m%s%s\x1b[0m', 'Error', testName);
+
+          if (count === limit) {
+            _app.produceTestReport(limit, successes, errors);
+          }
         }
       });
     });
--- test/unit.js
+++ test/unit.js
@@ -10,6 +10,9 @@ const lib = require('./../app/lib');
 // Container from unit test
 const unit = {};
 
+/**
+ * TEST PROP
+ */
 unit['prop should not throw an new error, when object does not exist'] = (done) => {
   const testObj = {};
 
@@ -46,6 +49,73 @@ unit['prop should return "null" when parameter does not exist'] = (done) => {
     lib.prop(['test, deep', 'doesNotExist'], testObj),
     null
   );
+  done();
+};
+
+/**
+ * TEST MAYBE
+ */
+unit['Maybe - should not throw an error, when call it without arguments'] = (done) => {
+  assert.doesNotThrow(() => {
+    lib.Maybe();
+    done();
+  }, TypeError);
+}
+
+unit['Maybe - should return 1'] = (done) => {
+  assert.equal(lib.Maybe(1).fold(() => null, res => res), 1);
+  done();
+}
+
+unit['Maybe - should return null'] = (done) => {
+  assert.equal(lib.Maybe(null).fold(() => null, () => 1), null);
+  done();
+}
+
+unit['Maybe - should return sum of two numbers'] = (done) => {
+
+  const sum = (a) => (b) => a + b;
+
+  const res = lib.Maybe(sum)
+    .ap(lib.Maybe(1))
+    .ap(lib.Maybe(2))
+    .fold(() => null, res => res);
+
+  assert.equal(res, 3);
+
+  done();
+}
+
+/**
+ * TEST EITHER
+ */
+const findColor = name => lib.Either(({
+  green: 'green',
+  blue: 'blue',
+  red: 'red'
+})[name]);
+
+unit['EITHER - should return green color'] = (done) => {
+  const color = findColor('green').fold(() => null, res => res);
+  assert.equal(color, 'green');
+  done();
+}
+
+unit['EITHER - should return "RED" color'] = (done) => {
+  const color = findColor('red').map(color => color.toUpperCase()).fold(() => null, res => res);
+  assert.equal(color, 'RED');
+  done();
+};
+
+unit['EITHER - should concat two colors with coma'] = (done) => {
+  const color = findColor('red').map(res => `${res},`).concat(findColor('blue')).fold(() => null, res => res);
+  assert.equal(color, 'red,blue');
+  done();
+};
+
+unit['EITHER - should return "YELLOW" color but throw error (example error)'] = (done) => {
+  const color = findColor('yellow').map(color => color.toUpperCase()).fold(() => null, res => res);
+  assert.equal(color, 'YELLOW');
   done();
 };
 
